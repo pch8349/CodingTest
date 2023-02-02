f927d4e247afd669f9eef49c06b87637a7eb51b5
pch8349 <chan449@naver.com>
2023-02-02 21:25:11 +0900

    02.02 Baekjoon solve

--- src/baekjoon/Main_2635.java
+++ src/baekjoon/Main_2635.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main_2635 {
@@ -15,13 +16,22 @@ public class Main_2635 {
         List<Integer> ret = new ArrayList<>();
 
         for(int i = N; i>=0; i--) {
-            ans = 1;
             List<Integer> list =  new ArrayList<>();
             list.add(N);
             list.add(i);
-            while(list.get(list.size()-1) >= 0) {
-
+            int index = 1;
+            while(list.get(index) >= 0) {
+                list.add(list.get(index-1)-list.get(index));
+                index++;
             }
+            if(ans<index) {
+                ans = index;
+                ret = (List<Integer>) ((ArrayList<Integer>) list).clone();
+            }
+        }
+        System.out.println(ans);
+        for(int i = 0; i<ret.size()-1; i++) {
+            System.out.print(ret.get(i)+" ");
         }
     }
 }
