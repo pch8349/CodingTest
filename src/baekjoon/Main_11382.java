e4cf4e4280048174f9ed7bae2646def05295d51e
pch8349 <chan449@naver.com>
2024-10-07 18:15:08 +0900

    새싹 문제 11382 solve

---
+++ src/baekjoon/Main_11382.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+public class Main_11382 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A+B+C);
+    }
+}
