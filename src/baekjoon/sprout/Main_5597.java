ecff12011869f3212d8ef99d70ed818bbb550fe1
pch8349 <chan449@naver.com>
2024-10-16 23:05:55 +0900

    새싹 배열 문제 5597 solve

---
+++ src/baekjoon/sprout/Main_5597.java
@@ -0,0 +1,18 @@
+package baekjoon.sprout;
+import java.io.*;
+public class Main_5597 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean student[] = new boolean[31];
+
+        for(int i = 0; i<28; i++){
+            student[Integer.parseInt(br.readLine())] = true;
+        }
+
+        for(int i = 1; i<=30; i++){
+            if(!student[i]) System.out.println(i);
+        }
+
+    }
+}
