acfd50405e09a3599946f2115d880431b958a32c
pch8349 <chan449@naver.com>
2025-01-10 23:33:20 +0900

    gold5 문제 12865 dp 냅색 문제

--- src/baekjoon/gold/Main_12865.java
+++ src/baekjoon/gold/Main_12865.java
@@ -2,39 +2,37 @@ package baekjoon.gold;
 import java.io.*;
 import java.util.*;
 public class Main_12865 {
-    static int N, K;
-    static int [][]s; // 무게, 가치
-    static int [][]dp;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        s = new int[N+1][2];
-        for(int i = 1; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            s[i][0] = Integer.parseInt(st.nextToken());
-            s[i][1] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int []W = new int[N+1];
+        int []V = new int[N+1];
+        int [][]dp = new int[N+1][K+1];
+
+        for(int i = 1; i<=N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new int[N+1][K+1];
-        for(int i = 1; i<=N; i++){
-            for(int j = 1; j<=K; j++){
-                dp[i][j] = dp[i-1][j];
-                if(j>=s[i][0]){
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-s[i][0]] + s[i][1]);
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= K; j++){
+
+                if(W[i]>j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
                 }
             }
         }
 
-
-        int ans = dp[N][K];
-
-        bw.write(String.valueOf(ans));
-        bw.flush();
-        bw.close();
+        System.out.println(dp[N][K]);
     }
 }
