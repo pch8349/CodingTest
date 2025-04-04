f9d9431dc9fbdabbf7e2fba0f3ab41d28c7a5ec6
pch8349 <chan449@naver.com>
2025-04-04 17:45:23 +0900

    gold3 class5 2623(위상정렬) 풀이 완료

--- src/baekjoon/gold/Main_2623.java
+++ src/baekjoon/gold/Main_2623.java
@@ -1,10 +1,23 @@
 package baekjoon.gold;
+
 import java.io.*;
 import java.util.*;
 public class Main_2623 {
+
+    static class Node{
+        int idx;
+        int n;
+        Node(int idx, int n){
+            this.idx = idx;
+            this.n = n;
+        }
+    }
+
     static int N, M;
     static List<Integer>[] childGraph;
-    static int[] nodeCnt;
+    static Node[] nodeCnt;
+    static final int fin = 999999;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,8 +26,11 @@ public class Main_2623 {
         M = Integer.parseInt(st.nextToken());
 
         childGraph = new List[N + 1];
-        nodeCnt = new int[N + 1];
-        for(int i = 1; i <= N; i++) childGraph[i] = new ArrayList<>();
+        nodeCnt = new Node[N + 1];
+        for(int i = 1; i <= N; i++) {
+            childGraph[i] = new ArrayList<>();
+            nodeCnt[i] = new Node(i, 0);
+        }
 
         for(int i = 0; i<M; i++){
             st = new StringTokenizer(br.readLine());
@@ -23,23 +39,40 @@ public class Main_2623 {
             for(int j = 0; j<a-1; j++){
                 int y = Integer.parseInt(st.nextToken());
                 childGraph[x].add(y);
-                nodeCnt[y]++;
+                nodeCnt[y].n++;
                 x = y;
             }
         }
 
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.n - o2.n);
 
+        for(int i = 1; i<=N; i++) pq.add(nodeCnt[i]); // 번호, 연결차수
 
-        for(int i = 1; i<=N; i++){
-            for(int j = 0; j<childGraph[i].size(); j++){
-                System.out.print(childGraph[i].get(j)+" ");
+        StringBuilder sb = new StringBuilder();
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            if(cur.n == fin){
+                break;
             }
-            System.out.println();
-        }
-        System.out.println("==========");
-        for(int i = 1; i<=N; i++){
-            System.out.println(nodeCnt[i]);
-        }
+            else if(cur.n > 0){
+                sb = new StringBuilder().append(0);
+                break;
+            } else {
+                sb.append(cur.idx).append("\n");
+                for(int i = 0; i<childGraph[cur.idx].size(); i++){
+                    nodeCnt[childGraph[cur.idx].get(i)].n -= 1;
+                }
+                nodeCnt[cur.idx].n = fin;
+            }
 
+            pq.clear();
+            for (int i = 1; i <= N; i++) {
+                if (nodeCnt[i].n != fin) {
+                    pq.add(nodeCnt[i]);
+                }
+            }
+        }
+        System.out.println(sb);
     }
 }
