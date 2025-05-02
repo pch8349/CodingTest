b11372c40d72aa22ae85b6d51a2abde5c5771c3b
pch8349 <chan449@naver.com>
2025-05-02 17:41:05 +0900

    gold3 11049 DP 문제 사실 이해 못함

--- src/baekjoon/class5/Main_11049.java
+++ src/baekjoon/class5/Main_11049.java
@@ -5,56 +5,31 @@ public class Main_11049 {
 
     static int N;
     static int[][] arr;
-    static Node [][]dpArr;
+    static long[][] dp;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
         arr = new int[N][2];
-        dpArr = new Node [N][N];
+        dp = new long[N][N];
 
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i<N-1; i++){
-            dpArr[i][i+1] = new Node(arr[i][0], arr[i+1][1], (long) arr[i][0] *arr[i][1]*arr[i+1][1]);
+        for (int len = 2; len <= N; len++){
+            for(int i = 0; i<=N - len; i++){
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k = i; k< j; k++){
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k+1][j] + (long) arr[i][0] * arr[k][1] * arr[j][1]);
+                }
+            }
         }
 
-        long ans = dp(0, N-1).v;
-
-        System.out.println(ans);
-    }
-
-    private static Node dp(int st, int end){
-        if(dpArr[st][end] != null){
-            return dpArr[st][end];
-        }
-        Node x = dp(st, end - 1);
-        Node y = dp(st + 1, end);
-        long xSum = x.v + (long) x.x * x.y * arr[end][1];
-        long ySum = y.v + (long) y.x  * y.y * arr[st][0];
-
-        if(xSum < ySum){
-            dpArr[st][end] = new Node(x.x, arr[end][1], xSum);
-            return dpArr[st][end];
-        } else {
-            dpArr[st][end] = new Node(arr[st][0], y.y, ySum);
-            return dpArr[st][end];
-        }
-    }
-
-    private static class Node{
-        public int x;
-        public int y;
-        public long v;
-        public Node(int x, int y, long v){
-            this.x = x;
-            this.y = y;
-            this.v = v;
-        }
+        System.out.println(dp[0][N-1]);
     }
 }
