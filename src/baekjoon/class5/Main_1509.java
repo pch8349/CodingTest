2d1f4131e71f90b9810f851e4c875e3b565c42c7
pch8349 <chan449@naver.com>
2025-07-11 17:26:41 +0900

    class5 gold1 1509 dp 완료

--- src/baekjoon/class5/Main_1509.java
+++ src/baekjoon/class5/Main_1509.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main_1509 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String S = br.readLine();
         int N = S.length();
 
